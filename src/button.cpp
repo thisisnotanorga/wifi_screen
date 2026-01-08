2fe677325e19aa271589accd0d6d6474d315bf89
Merlin.rce <racinemerlin@gmail.com>
2026-01-08 07:56:45 +0100

    refactor button and improve the debounce

--- src/button.cpp
+++ src/button.cpp
@@ -1,11 +1,40 @@
 #include "button.h"
 
-void button_init(int pin)
-{
-pinMode(pin, INPUT_PULLUP);
+
+//creat button structure contains all the variables needed for button the button
+struct button {
+  uint8_t pin;
+  bool physical_state;           // mostly pull up so HIGH = not pressed
+  bool last_read;           // Last reading to check further if it changed
+  unsigned long  lastChangeMs ; // timer debounce
+};
+
+void button_init(button &b, uint8_t pin) {
+  b.pin = pin;
+  pinMode(b.pin, INPUT_PULLUP);
+  b.physical_state = HIGH; // état initial au repos
+  b.last_read = HIGH;
+  b.lastChangeMs = millis();
 }
 
-// pas pertinent       next... find a way of button init qui cree tout last state digital read stocker dans une variable
 
-// next faire aussi une fonction button is pressed qui retourne true si appuye et false sinon, en utilisant le last state pour eviter les rebonds
+// true UNE seule fois au moment de l'appui (front)
+bool button_pressed(button &b, unsigned long debounceMs) {
+  bool reading = digitalRead(b.pin);
 
+  if (reading != b.last_read) {
+    b.last_read = reading;
+    b.lastChangeMs = millis();
+  }
+
+  if (millis() - b.lastChangeMs >= debounceMs) {
+    if (reading != b.physical_state) {
+      b.physical_state = reading;
+
+      // INPUT_PULLUP: appuyé = LOW
+      if (b.physical_state == LOW) return true;
+    }
+  }
+
+  return false;
+}
